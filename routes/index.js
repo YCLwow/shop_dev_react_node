0d8fb1f91bb66aefb14c0a87e29ba79d7786ecfa
lyc <lyc>
2022-09-19 15:20:12 +0800

    修改接口路径

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
  * @Author: liuyichen
  * @Date: 2022-07-27 16:28:21
  * @LastEditors: liuyichen
- * @LastEditTime: 2022-09-16 14:46:07
+ * @LastEditTime: 2022-09-19 10:26:27
  * @FilePath: \代码仓库\shop_dev_react_node\routes\index.js
  * @Description: 
  * 
@@ -16,8 +16,6 @@ const app=express()
 const jwt =require('jsonwebtoken')
 // 自定义秘钥 
 const secretkey='secret12345'
-
-
 
 var router = express.Router();
 // sql对象
@@ -52,6 +50,15 @@ router.get('/login', function(req, res, next) {
 	});
 });
 
+/**
+ * @description: 商品查询 C+W+t
+ * @param {*} getProductList
+ * @param {*} function
+ * @param {*} res
+ * @param {*} next
+ * @param {*} productType  产品类型
+ * @return {*}
+ */
 router.post('/productList',function(req,res,next){
   let sql =`select * from product_lists where productType in ( ?,?,? )`
   let sqlValue  =req.body.productType
